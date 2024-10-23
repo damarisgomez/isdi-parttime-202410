d819700c6d9d4a5efa36840340df6f20470fd63c
damarisgomez <damgc.gv@gmail.com>
2024-10-23 16:02:08 +0200

    add array & dorraymon at method test and implementation #10

--- staff/damaris-gomez/playground/dorraymon/Dorraymon.prototype.at.test.js
+++ staff/damaris-gomez/playground/dorraymon/Dorraymon.prototype.at.test.js
@@ -1,44 +1,42 @@
-console.log('TEST Array.prototype.at')
-//'case get ELEMENT at index 3'// 
+function Dorraymon() {
+    this.length = 0
+}
+
+
+Dorraymon.prototype.at = function (index) {
+    if (index < 0) {
+        var newIndex = this.length + index
+
+        return this[newIndex]
+    } else return this[index]
+}
+// para que funcion el ejercio ('-2,-3,-1')(-10), se ha tenido que ajustar.
+
+
+console.log('TEST Dorraymon.prototype.at')
+
 console.log('CASE get car at index 3')
 
-// var cars = ['lambo', 'f350','911', 'm5', 'hummer'] es un atajo//
-//lo que realmente se ve por dentro: // 
-
-var cars = new Array
-cars[0] = 'lambo'
-cars[1] = 'f350'
+var cars = new Dorraymon
+cars[0] = 'Lambo'
+cars[1] = 'F350'
 cars[2] = '911'
 cars[3] = 'm5'
 cars[4] = 'hummer'
-var car = car.at(3)
+cars.length = 5
+var car = cars.at(3)
 console.log(car)
 //m5
-
-//array//
-a = []
-a = new Array
-
-/* [array]
-      length: 0
-      Prototyoe
- */
-
-function Pepe() {
-    this.name = 'Pepe Grillo'
-}
-var pepe = new Pepe
-console.log(pepe)
-
 
 
 console.log('CASE get function at index 2')
 
-var funs = new Array
+var funs = new Dorraymon
 funs[0] = function () { return 'Zero' }
 funs[1] = function () { return 'One' }
 funs[2] = function () { return 'Two' }
 funs[3] = function () { return 'Three' }
+funs.length = 4
 var fun = funs.at(2)
 console.log(fun())
 //two
@@ -46,11 +44,12 @@ console.log(fun())
 
 console.log('CASE get item at index -2, -3, -1')
 
-var cart = new Array
+var cart = new Dorraymon
 cart[0] = { brand: "Adidas", model: "Niza", size: 43, price: 70, quantity: 2 };
 cart[1] = { brand: "Puma", model: "Gatito", size: "L", price: 20, quantity: 3 };
 cart[2] = { brand: "Nike", model: "Chachi", size: 44, price: 15, quantity: 6 };
 var item = cart.at(-2)
+cart.length = 3
 console.log(item)
 // { brand: "Puma", model: "Gatito", size: "L", price: 20, quantity: 3 }
 var itema = cart.at(-3)
@@ -63,23 +62,23 @@ console.log(item)
 
 console.log('CASE get item at index -10')
 
-var cart = new Array
+var cart = new Dorraymon
 cart[0] = { brand: "Adidas", model: "Niza", size: 43, price: 70, quantity: 2 };
 cart[1] = { brand: "Puma", model: "Gatito", size: "L", price: 20, quantity: 3 };
 cart[2] = { brand: "Nike", model: "Chachi", size: 44, price: 15, quantity: 6 };
 var itema = cart.at(-10)
+cart.length = 3
 console.log(item)
 // undefined
 
 
 console.log('CASE get item at index 10')
 
-var cart = new Array
+var cart = new Dorraymon
 cart[0] = { brand: "Adidas", model: "Niza", size: 43, price: 70, quantity: 2 };
 cart[1] = { brand: "Puma", model: "Gatito", size: "L", price: 20, quantity: 3 };
 cart[2] = { brand: "Nike", model: "Chachi", size: 44, price: 15, quantity: 6 };
 var itema = cart.at(10)
+cart.length = 3
 console.log(item)
 // undefined
-
-// TDD Test-Driven Development , técnica que consiste en testear y corregir errores.
